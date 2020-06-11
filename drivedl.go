aba074e2d9038e22c6b898acccfe463261e5b9ca
jaskaranSM <beerabassian32@gmail.com>
2020-06-11 12:52:49 +0530

    make fileid a positonal arg

--- drivedl.go
+++ drivedl.go
@@ -2,59 +2,54 @@ package main
 
 import (
 	"driveHelper"
-	"os"
-	"log"
 	"errors"
+	"fmt"
+	"log"
+	"os"
 	"runtime"
+
 	"github.com/urfave/cli"
 )
 
 func downloadCallback(c *cli.Context) error {
-	if c.String("fileId") == "" {
-		return errors.New("fileId arg is empty, exiting..")
+	arg := c.Args().Get(0)
+	if arg == "" {
+		return errors.New(fmt.Sprintf("Required argument <fileid> is missing. \nUsage: %s\nFor more info --help ", c.App.UsageText))
 	}
 	GD := driveHelper.NewDriveClient()
 	GD.Init()
 	GD.Authorize()
 	GD.SetConcurrency(c.Int("conn"))
-	GD.Download(c.String("fileId"),c.String("path"))
+	GD.Download(arg, c.String("path"))
 	return nil
 }
 
-func main(){
+// ./drivedl --conn 4 --path "dls/dl/as" 1Zh5fQ6e6U1ySMA15vKMvDQcIROqlHcpp
+func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	app := cli.NewApp()
-    app.Name = "Google Drive Downloader"
-    app.Usage = "A minimal Google Drive Downloader written in Go."
-    app.Authors = []*cli.Author{
-        {Name:"JaskaranSM",},
-    }
-    downloadFlags := []cli.Flag{
-        &cli.StringFlag{
-            Name:"path",
-            Usage:"Folder path to store the download.",
-            Value:".",
-        },
-        &cli.StringFlag{
-            Name:"fileId",
-            Usage:"Id of Google Drive file/folder.",
-        },
-        &cli.IntFlag{
-            Name:"conn",
-            Usage:"Number of Concurrent File Downloads.",
-            Value:2,
-        },
-    }
-    app.Commands = cli.Commands{
-        {
-            Name:"dl",
-            Usage:"Download Google Drive file/folder to local.",
-            Flags:downloadFlags,
-            Action:downloadCallback,
-        },
-    }
-    err := app.Run(os.Args)
-    if err != nil {
-        log.Fatal(err)
-    }
+	app.Name = "Google Drive Downloader"
+	app.Usage = "A minimal Google Drive Downloader written in Go."
+	app.UsageText = "drivedl [global options] <fileid>"
+	app.Authors = []*cli.Author{
+		{Name: "JaskaranSM"},
+	}
+	app.Action = downloadCallback
+	app.Version = "1.0"
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "path",
+			Usage: "Folder path to store the download.",
+			Value: ".",
+		},
+		&cli.IntFlag{
+			Name:  "conn",
+			Usage: "Number of Concurrent File Downloads.",
+			Value: 2,
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
